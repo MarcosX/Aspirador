818faa552e40539aeaa3e98182afea98f0aa62ed
MarcosX <marcos.uece.comp@gmail.com>
2011-10-15 08:10:47 -0300

    Testes

--- src/br/intcomp/main/Main.java
+++ src/br/intcomp/main/Main.java
@@ -3,9 +3,12 @@ package br.intcomp.main;
 public class Main {
 
 	public static void main(String[] args) {
-		AmbienteTesteFactory fabrica = new AmbienteTesteFactory(10, 30);
-		fabrica.setQtdIteracoes(100);
-		fabrica.criarAmbienteAgenteRandom(0.5);
-		fabrica.criarAmbienteAgenteEstadoInterno(0.5);
+//		AmbienteTesteFactory fabrica = new AmbienteTesteFactory(10, 30);
+//		fabrica.setQtdIteracoes(100);
+//		fabrica.criarAmbienteAgenteRandom(0.5);
+//		fabrica.criarAmbienteAgenteEstadoInterno(0.5);
+		
+		AmbienteGraficoFactory fabrica = new AmbienteGraficoFactory(5, 7);
+		fabrica.criarAmbienteAgenteEstadoInterno(0.7);
 	}
 }
